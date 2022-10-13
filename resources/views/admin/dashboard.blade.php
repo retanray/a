48f8655a44ffe4a42fe69328da9ac2835affbcc9
hyunho <affiwtaoe@ideakey.co.kr>
2022-10-13 09:56:01 +0900

    feat: adminlte install

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,19 @@
-admin
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+  <h1>Dashboard</h1>
+@stop
+
+@section('content')
+  <p>Welcome</p>
+@stop
+
+@section('css')
+  <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+  <script> console.log('11'); </script>
+@stop
